b707176d3336d94586ca0f4689bb5b29b4bdcfe0
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-23 14:20:26 -0500

    Created get route

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const toDoRouter = express.Router();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-app.use('/todo', toDoRouter)
+app.use('/todo', toDoRouter);
+
+toDoRouter.get('/' , (req, res) => {
+    console.log('GET /todo');
+    let queryText = `
+    SELECT * FROM "todo"
+    `;
+    pool.query(queryText)
+    .then((dbResult) => {
+        res.send(dbResult.rows);
+    })
+    .catch((dbError) => {
+        console.log('error in GET /todo db request:', dbError);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = toDoRouter;
